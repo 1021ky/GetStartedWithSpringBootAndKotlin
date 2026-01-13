8b274d4019c650750ada2e7a0dbed106aaeaa576
Keisuke Yamanaka <2500024+1021ky@users.noreply.github.com>
2026-01-13 21:09:03 +0900

    dataクラスをためす

---
+++ src/main/kotlin/personal/keisuke/yamanaka/getstarted/Message.kt
@@ -0,0 +1,6 @@
+package personal.keisuke.yamanaka.getstarted
+
+// data classは主にデータを保持する専用
+// valはreadonly
+// ?はread only
+data class Message (val id: String?, val text: String)
--- src/main/kotlin/personal/keisuke/yamanaka/getstarted/MessageController.kt
+++ src/main/kotlin/personal/keisuke/yamanaka/getstarted/MessageController.kt
@@ -6,6 +6,14 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class MessageController {
-    @GetMapping("/")
-    fun index(@RequestParam("name") name: String) = "Hello, $name!"
+    @GetMapping
+    fun listMessages() = listOf(
+        Message("1", "Hello!"),
+        Message("2", "Bonjour!"),
+        Message("3", "Privet!"),
+    )
+
+//    @GetMapping("/")
+//    fun index(@RequestParam("name") name: String) = "Hello, $name!"
+
 }
